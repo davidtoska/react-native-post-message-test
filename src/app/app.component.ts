57b3184514d01c48ea46991731ce162ce423f9a1
David Toska <david.toska@outlook.com>
2022-02-03 15:40:42 +0100

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (message: any) => void;
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'react-native-post-message-test';
+
+  ngOnInit(): void {
+    setInterval(() => {
+      console.log("Posting message to react-native webview");
+      //
+      if("ReactNativeWebView" in window) {
+        window.ReactNativeWebView.postMessage("Hello from ispe-app");
+      }
+    }, 2000)
+  }
+
 }
