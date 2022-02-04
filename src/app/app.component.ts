f7ed8f5b1597829a10047215601ebbb3f771b283
David Toska <david.toska@outlook.com>
2022-02-04 06:46:18 +0100

    added firebase-hosting since netlify would not build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 
-// declare global {
-//   interface Window {
-//     ReactNativeWebView: {
-//       postMessage: (message: any) => void;
-//     }
-//   }
-// }
-// window.ReactNativeWebView = window.ReactNativeWebView || {postMessage: (message) => {
-//     console.log(message)
-// }};
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (message: any) => void;
+    }
+  }
+}
+window.ReactNativeWebView = window.ReactNativeWebView || {postMessage: (message) => {
+    console.log("Seems like the ReactNativeWebView is not available on the window object");
+}};
 
 @Component({
   selector: 'app-root',
@@ -23,9 +23,9 @@ export class AppComponent implements OnInit{
     setInterval(() => {
       console.log("Posting message to react-native webview");
 
-      // if("ReactNativeWebView" in window) {
-      //   window?.ReactNativeWebView?.postMessage("Hello from ispe-app");
-      // }
+      if("ReactNativeWebView" in window) {
+        window?.ReactNativeWebView?.postMessage("Hello from ispe-app");
+      }
     }, 2000)
   }
 
